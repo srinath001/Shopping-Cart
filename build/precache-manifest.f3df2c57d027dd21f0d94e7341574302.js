f5f7b9a7ed43e341dedec3c86f978eb511d82e55
srinath001 <srinath.vasudev@gmail.com>
2022-01-30 19:08:56 +0530

    Add files via upload

---
+++ build/precache-manifest.f3df2c57d027dd21f0d94e7341574302.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf186c3df6f3caf32b28",
+    "url": "/static/css/main.e1b08b61.chunk.css"
+  },
+  {
+    "revision": "bf186c3df6f3caf32b28",
+    "url": "/static/js/main.bf186c3d.chunk.js"
+  },
+  {
+    "revision": "99d7c59f45359249397b",
+    "url": "/static/js/1.99d7c59f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "8a9fad03937f53500ec6c0d24b57eab2",
+    "url": "/index.html"
+  }
+];
