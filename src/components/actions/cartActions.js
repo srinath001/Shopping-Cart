04dfdeef4130b581c1f6b8c70f7303b25b3f871d
srinath001 <srinath.vasudev@gmail.com>
2022-01-30 20:04:57 +0530

    Update cartActions.js

--- src/components/actions/cartActions.js
+++ src/components/actions/cartActions.js
@@ -1,6 +1,6 @@
 
 
-import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING} from './action-types/cart-actions'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY} from './action-types/cart-actions'
 
 //add cart action
 export const addToCart= (id)=>{
